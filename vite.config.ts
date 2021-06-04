a8fb8882f9796da19e4302bd61fb11e6b87b732b
Epimodev <godin.fred8@gmail.com>
2021-06-04 14:23:34 +0200

    deploy version 1.0.2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./src",
-  base: "/card-design-studio",
+  base: "/card-design-studio/",
   plugins: [reactRefresh()],
 })
