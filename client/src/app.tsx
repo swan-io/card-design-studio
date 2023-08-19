f212ac72d471191338ffbb1c4dad7701118d4363
Frédéric Godin <frederic@MacBook-Pro-de-Frederic.local>
2023-08-19 14:39:36 +0200

    set empty tiles for steps

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,14 +1,55 @@
 import { LoadingView } from "@swan-io/lake/src/components/LoadingView";
-import { Suspense, lazy } from "react";
-import { View } from "react-native";
+import { Suspense, lazy, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { ColorStep, LogoStep, NameStep, WelcomeStep } from "./components/ConfigSteps";
 const Card3dScene = lazy(() => import("./components/Card3dScene"));
 
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
 export const App = () => {
+  const [step, setStep] = useState<ConfigStep>("welcome");
+  const [name, setName] = useState("");
+  const [logo, setLogo] = useState<SVGElement | null>(null);
+  const [logoScale, setLogoScale] = useState(1);
+  const [color, setColor] = useState<CardConfig["color"]>("Silver");
+
   return (
-    <View style={{ width: "100%", height: "100%" }}>
+    <View style={styles.container}>
       <Suspense fallback={<LoadingView />}>
-        <Card3dScene />
+        <Card3dScene step={step} ownerName={name} color={color} logo={logo} logoScale={logoScale} />
       </Suspense>
+
+      <WelcomeStep visible={step === "welcome"} onStart={() => setStep("name")} />
+
+      <NameStep
+        visible={step === "name"}
+        name={name}
+        onNameChange={setName}
+        onNext={() => setStep("logo")}
+      />
+
+      <LogoStep
+        visible={step === "logo"}
+        logo={logo}
+        logoScale={logoScale}
+        onLogoChange={setLogo}
+        onLogoScaleChange={setLogoScale}
+        onPrevious={() => setStep("name")}
+        onNext={() => setStep("color")}
+      />
+
+      <ColorStep
+        visible={step === "color"}
+        color={color}
+        onColorChange={setColor}
+        onPrevious={() => setStep("logo")}
+        onNext={() => setStep("completed")}
+      />
     </View>
   );
 };
