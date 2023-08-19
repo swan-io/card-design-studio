d0fb45de44ffb00bccc7f26291ba57fde1f64d14
Frédéric Godin <frederic@MacBook-Pro-de-Frederic.local>
2023-08-19 15:51:28 +0200

    welcome page integration

--- client/src/components/ConfigSteps.tsx
+++ client/src/components/ConfigSteps.tsx
@@ -1,13 +1,37 @@
+import { Box } from "@swan-io/lake/src/components/Box";
 import { LakeButton } from "@swan-io/lake/src/components/LakeButton";
+import { LakeHeading } from "@swan-io/lake/src/components/LakeHeading";
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
 import { LakeTextInput } from "@swan-io/lake/src/components/LakeTextInput";
+import { Space } from "@swan-io/lake/src/components/Space";
+import { SwanLogo } from "@swan-io/lake/src/components/SwanLogo";
 import { Tile } from "@swan-io/lake/src/components/Tile";
 import { TransitionView } from "@swan-io/lake/src/components/TransitionView";
-import { animations } from "@swan-io/lake/src/constants/design";
-import { StyleSheet } from "react-native";
+import { animations, colors } from "@swan-io/lake/src/constants/design";
+import { StyleSheet, View } from "react-native";
 import { t } from "../utils/i18n";
 
 const styles = StyleSheet.create({
+  welcomeContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: colors.gray[0],
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcomeStep: {
+    maxWidth: 700,
+  },
+  welcomeSwanLogo: {
+    width: 98,
+    height: 22,
+  },
+  startButton: {
+    alignSelf: "flex-start",
+  },
   stepContainer: {
     position: "absolute",
     bottom: 0,
@@ -22,11 +46,36 @@ type WelcomeStepProps = {
 };
 
 export const WelcomeStep = ({ visible, onStart }: WelcomeStepProps) => (
-  <TransitionView style={styles.stepContainer} {...animations.fadeAndSlideInFromRight}>
+  <TransitionView style={styles.welcomeContainer} {...animations.fadeAndSlideInFromRight}>
     {visible ? (
-      <Tile>
-        <LakeButton onPress={onStart}>{t("welcome.button")}</LakeButton>
-      </Tile>
+      <View style={styles.welcomeStep}>
+        <Box direction="row" alignItems="center">
+          <SwanLogo style={styles.welcomeSwanLogo} />
+          <Space width={16} />
+
+          <LakeHeading level={1} variant="h1">
+            {t("welcome.title")}
+          </LakeHeading>
+        </Box>
+
+        <Space height={16} />
+
+        <LakeHeading level={2} variant="h3" color={colors.gray[600]}>
+          {t("welcome.subtitle")}
+        </LakeHeading>
+
+        <Space height={32} />
+
+        <LakeButton
+          color="live"
+          icon="arrow-right-filled"
+          iconPosition="end"
+          style={styles.startButton}
+          onPress={onStart}
+        >
+          {t("welcome.button")}
+        </LakeButton>
+      </View>
     ) : null}
   </TransitionView>
 );
