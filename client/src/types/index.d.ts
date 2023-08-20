6fa1ce8b0ba69e620001169ee51fb2ee924a0ec7
Frédéric Godin <frederic@MacBook-Pro-de-Frederic.local>
2023-08-20 09:58:51 +0200

    setup router

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-type ConfigStep = "welcome" | "name" | "logo" | "color" | "completed";
+type ConfigStep = "welcome" | "name" | "logo" | "color" | "completed" | "share";
 
 type CardConfig = {
   name: string;
