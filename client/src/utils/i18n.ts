1d12b136368e545a95a1c514d98e75c1b984892b
Frédéric Godin <frederic@MacBook-Pro-de-Frederic.local>
2023-08-20 15:50:01 +0200

    comment languages

--- client/src/utils/i18n.ts
+++ client/src/utils/i18n.ts
@@ -3,15 +3,10 @@ import {
   LANGUAGE_FALLBACK,
   getLanguagesHelpers,
 } from "@swan-io/shared-business/src/utils/languages";
-import translationDE from "../locales/de.json";
 import translationEN from "../locales/en.json";
-import translationES from "../locales/es.json";
-import translationFR from "../locales/fr.json";
-import translationIT from "../locales/it.json";
-import translationNL from "../locales/nl.json";
-import translationPT from "../locales/pt.json";
 
-const supportedLanguages = ["en", "es", "de", "fr", "it", "nl", "pt"] as const;
+// const supportedLanguages = ["en", "es", "de", "fr", "it", "nl", "pt"] as const;
+const supportedLanguages = ["en"] as const;
 type SupportedLanguage = (typeof supportedLanguages)[number];
 
 export type TranslationKey = keyof typeof translationEN;
@@ -23,34 +18,34 @@ type Locale = {
 };
 
 const locales: Record<SupportedLanguage, () => Locale> = {
-  de: () => ({
-    language: "de",
-    translations: translationDE,
-  }),
+  // de: () => ({
+  //   language: "de",
+  //   translations: translationDE,
+  // }),
   en: () => ({
     language: "en",
     translations: translationEN,
   }),
-  es: () => ({
-    language: "es",
-    translations: translationES,
-  }),
-  fr: () => ({
-    language: "fr",
-    translations: translationFR,
-  }),
-  it: () => ({
-    language: "it",
-    translations: translationIT,
-  }),
-  nl: () => ({
-    language: "nl",
-    translations: translationNL,
-  }),
-  pt: () => ({
-    language: "pt",
-    translations: translationPT,
-  }),
+  // es: () => ({
+  //   language: "es",
+  //   translations: translationES,
+  // }),
+  // fr: () => ({
+  //   language: "fr",
+  //   translations: translationFR,
+  // }),
+  // it: () => ({
+  //   language: "it",
+  //   translations: translationIT,
+  // }),
+  // nl: () => ({
+  //   language: "nl",
+  //   translations: translationNL,
+  // }),
+  // pt: () => ({
+  //   language: "pt",
+  //   translations: translationPT,
+  // }),
 };
 
 export const { getBestLocale, getFirstSupportedLanguage, setPreferredLanguage } =
