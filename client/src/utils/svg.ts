0c0df6925dc667c2c48619aa6978f582c868f59f
Frédéric Godin <frederic@MacBook-Pro-de-Frederic.local>
2023-08-27 17:35:19 +0200

    add help link + button to set Swan's logo

--- client/src/utils/svg.ts
+++ client/src/utils/svg.ts
@@ -202,6 +202,29 @@ export const getMonochromeSvg = (svg: SVGElement, color: "white" | "black"): SVG
   return newSvg;
 };
 
+// Size and path copied from https://github.com/swan-io/lake/blob/main/packages/lake/src/components/SwanLogo.tsx
+export const createSwanLogoSvg = (): SVGElement => {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const width = 45;
+  const height = 10;
+
+  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svg.setAttribute("width", width.toString());
+  svg.setAttribute("height", height.toString());
+  svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
+
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute(
+    "d",
+    "M41.01 0c2.2 0 3.66 1.6 3.66 3.82v5.95H42.9V3.9c0-1.28-1-2.19-2.23-2.19a2.33 2.33 0 00-2.37 2.4v5.65h-1.83V.27h1.83v.92A3.4 3.4 0 0141 0zM30.77 9.73H29.2c-2.65 0-4.53-2.27-4.53-4.86A4.81 4.81 0 0129.44 0c2.92 0 4.93 1.74 4.93 4.45v5.32h-1.71V4.45c0-1.72-1.2-2.76-3.22-2.76a3.11 3.11 0 00-3.06 3.18c0 1.72 1.2 3.17 2.82 3.17h1.57v1.7zm-14.37-7l-2.25 7.04h-2.1L9.07.27h1.96l2.11 7.16L15.45.27h1.9l2.3 7.16L21.76.27h1.96l-3 9.5h-2.1zM4.14 10C1.94 10 .54 8.82.36 6.93L.33 6.7h1.79l.02.18c.15.98.84 1.48 2.07 1.48s1.93-.54 1.93-1.38c0-.82-.5-1.1-2.15-1.42l-.43-.09C1.54 5.05.64 4.41.64 2.76.64 1.1 2.1 0 4.18 0 6.06 0 7.5 1.12 7.65 2.83l.02.23H5.9l-.02-.19c-.12-.79-.72-1.25-1.79-1.25-1.03 0-1.68.47-1.68 1.14 0 .68.47.92 1.99 1.2l.6.12c2 .4 2.93 1.1 2.93 2.83C7.93 8.76 6.4 10 4.14 10z",
+  );
+  path.setAttribute("fill", "#000");
+
+  svg.appendChild(path);
+
+  return svg;
+};
+
 export const createSvgImage = (svg: SVGElement): HTMLImageElement => {
   const svgSize = getSvgSize(svg);
   const base64Uri = convertSvgToBase64Uri(svg);
