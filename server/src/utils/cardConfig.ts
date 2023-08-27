a73c6f9a49d900de3fb59bd23b747f9bddface83
Frédéric Godin <frederic@MacBook-Pro-de-Frederic.local>
2023-08-27 14:57:24 +0200

    setup save config implementation based on file system

--- server/src/utils/cardConfig.ts
+++ server/src/utils/cardConfig.ts
@@ -1,21 +1,28 @@
 import { Option } from "@swan-io/boxed";
 import deburr from "lodash/deburr";
 import snakeCase from "lodash/snakeCase";
+import fs from "node:fs/promises";
+import path from "node:path";
 import { z } from "zod";
 
 export const cardConfigSchema = z.object({
   name: z.string(),
   logo: z.string(),
-  logoScale: z.number(),
+  logoScale: z.number().min(0).max(1),
   color: z.union([z.literal("Silver"), z.literal("Black")]),
 });
 
 export type CardConfig = z.infer<typeof cardConfigSchema>;
 
-const isConfigExists = async (_id: string): Promise<boolean> => {
-  // TODO check if config exists in database
-  await new Promise(resolve => setTimeout(resolve, 100));
-  return false;
+const uploadFolderPath = path.resolve(__dirname, "../../uploads");
+
+const isConfigExists = async (id: string): Promise<boolean> => {
+  // First implementation with file system, will be replaced by S3 bucket
+  const filePath = path.resolve(__dirname, uploadFolderPath, `${id}.json`);
+  return fs
+    .stat(filePath)
+    .then(() => true)
+    .catch(() => false);
 };
 
 const generateConfigId = async (name: string): Promise<string> => {
@@ -38,13 +45,30 @@ const generateConfigId = async (name: string): Promise<string> => {
 export const saveCardConfig = async (cardConfig: CardConfig): Promise<string> => {
   const id = await generateConfigId(cardConfig.name);
 
-  // TODO: save config to database
+  // First implementation with file system, will be replaced by S3 bucket
+  const folderExists = await fs
+    .stat(uploadFolderPath)
+    .then(() => true)
+    .catch(() => false);
+  if (!folderExists) {
+    await fs.mkdir(uploadFolderPath);
+  }
+
+  const filePath = path.resolve(uploadFolderPath, `${id}.json`);
+  await fs.writeFile(filePath, JSON.stringify(cardConfig), { encoding: "utf-8" });
 
   return id;
 };
 
-export const getCardConfig = async (_id: string): Promise<Option<CardConfig>> => {
-  // TODO: get config from database
-  await new Promise(resolve => setTimeout(resolve, 100));
-  return Option.None();
+export const getCardConfig = async (id: string): Promise<Option<CardConfig>> => {
+  try {
+    // First implementation with file system, will be replaced by S3 bucket
+    const filePath = path.resolve(__dirname, uploadFolderPath, `${id}.json`);
+    const configFile = await fs.readFile(filePath, { encoding: "utf-8" });
+    const config = cardConfigSchema.parse(JSON.parse(configFile));
+
+    return Option.Some(config);
+  } catch {
+    return Option.None();
+  }
 };
