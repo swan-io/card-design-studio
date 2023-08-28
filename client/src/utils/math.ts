fad030200ffb5625f5f9a3c917d108895e78b0fe
Frédéric Godin <frederic@MacBook-Pro-de-Frederic.local>
2023-08-28 14:02:58 +0200

    setup functions to animate camera

---
+++ client/src/utils/math.ts
@@ -0,0 +1,22 @@
+type InterpolateParams = {
+  range: [number, number];
+  output: [number, number];
+};
+
+export const clamp =
+  (min: number, max: number) =>
+  (value: number): number =>
+    Math.max(Math.min(value, max), min);
+
+export const interpolate = ({ range, output }: InterpolateParams): ((value: number) => number) => {
+  return (value: number): number => {
+    const inputRange = range[1] - range[0];
+    const outputRange = output[1] - output[0];
+
+    const valueWithoutOffset = value - range[0];
+    const outputWithoutOffset = (outputRange * valueWithoutOffset) / inputRange;
+    const outputValue = outputWithoutOffset + output[0];
+
+    return outputValue;
+  };
+};
