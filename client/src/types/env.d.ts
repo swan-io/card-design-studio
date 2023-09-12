772dec331f8f0834d61d973179f48238bf67a3de
Frédéric Godin <frederic@MacBook-Pro-de-Frederic.local>
2023-09-12 21:47:42 +0200

    add env.js endpoint

---
+++ client/src/types/env.d.ts
@@ -0,0 +1,5 @@
+// this is generated on server side in server/src/index.ts
+// and injected into the client side in client/src/index.html
+declare const __env: {
+  CLIENT_GOOGLE_TAG_MANAGER_ID: string;
+};
