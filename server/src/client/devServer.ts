a0f385878dad146e4190afb10651df7ecaa4f689
Frederic <frederic.godin@swan.io>
2026-07-01 15:48:32 +0200

    upgrade vite for cve

--- server/src/client/devServer.ts
+++ server/src/client/devServer.ts
@@ -1,8 +1,13 @@
 import http from "node:http";
 import path from "pathe";
 
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 export async function createViteDevServer() {
   const liveReloadServer = http.createServer();
+  // @ts-expect-error: Vite is installed at root level, we import it dynamically only when running the dev server
   const { createServer } = await import("vite");
   const { default: getPort } = await import("get-port");
   const mainServerPort = await getPort();
